c11e6bd963014d882bd971e83eb3265172d065ea
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-01-04 15:20:35 -0500

    Footer added

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer-div">
+            <p className="footer-div__p">Something will eventually go here</p>
+        </div>
+    )
+}
+
+export default Footer;
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <div className="navbar-div">
             <div className="navbar-div__left-group">
-                <button className="logo-button">Logo Goes Here</button>
+                <a className="navbar_anchor_button" href="/"><button className="logo-button">Logo Goes Here</button></a>
                 <button className="nav-bar-button">Techniques</button>
                 <button className="nav-bar-button">Benefits</button>
                 <button className="nav-bar-button">FAQ</button>
@@ -15,7 +15,7 @@ const NavBar = () => {
             </div>
             <div className="navbar-div__right-group">
                 <button className="nav-bar-button">Sign In</button>
-                <button className="nav-bar-button">Create Account</button>
+                <a className="navbar_anchor_button" href="/login/register"><button className="nav-bar-button">Create Account</button></a>
                 <button className="professionals-button">Professionals</button>
             </div>
         </div>
