ac585bad5aa60fb25df44100fdf581fa6939bbed
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-01-05 15:29:38 -0500

    added a user model and user route

--- back_end/__init__.py
+++ back_end/__init__.py
@@ -1,13 +1,28 @@
 import os
 from flask import Flask
 from flask_login import LoginManager, current_user
+from .models import db, User
+from flask_migrate import Migrate
+from back_end.api.user_routes import user_routes
+from back_end.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 login_manager = LoginManager(app)
-
 db.init_app(app)
+migrate = Migrate(app, db)
+
+app.register_blueprint(user_routes, url_prefix='/api/users')
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
 
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
---
+++ back_end/api/user_routes.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, jsonify, session, request
+from back_end.models import User, db
+from flask_login import login_user, LoginManager, current_user, logout_user,login_required
+
+user_routes = Blueprint('users', __name__)
+
+@user_routes.route('/register', methods=['POST'])
+def register():
+
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+
+    if not email or not password:
+        return {"errors": ["Missing email or password"]}, 400
+    
+    check_email = User.query.filter(User.email==email).all()
+    
+    if check_email:
+        return {"errors": ["Email is already in use"]}, 401
+    
+    new_user = User(
+        email=email,
+        password=password
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    login_user(new_user)
+    return {"current_user_id": current_user.id,
+    "current_user": current_user.to_dict()}
+    
