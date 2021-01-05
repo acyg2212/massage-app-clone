707774876738c82dd97d956da299782d17aff6d1
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2021-01-05 11:29:50 -0500

    pulling from github:

--- back_end/__init__.py
+++ back_end/__init__.py
@@ -5,10 +5,9 @@ from flask_login import LoginManager, current_user
 app = Flask(__name__)
 login_manager = LoginManager(app)
 
+db.init_app(app)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
